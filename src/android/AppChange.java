f5daddb72e5b1732468c2500b9020429e40670f9
pearl <lu92500@yahoo.com.tw>
2015-11-26 16:27:39 +0800

    My first backup

--- src/android/AppChange.java
+++ src/android/AppChange.java
@@ -1,23 +1,80 @@
 package tw.com.bais;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 
 public class AppChange extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if(action.equals("checkAvailability")) {
+        if(action.equals("check")) {
             String uri = args.getString(0);
-            this.checkAvailability(uri, callbackContext);
+            String non = args.getString(1);
+            this.checkAvailability(uri, non, callbackContext);
             return true;
         }
+        
+        if(action.equals("Inport")) {
+        	Intent intent = this.cordova.getActivity().getIntent();
+        	String scheme = intent.getScheme();
+        	if( scheme != null ){
+            	callbackContext.success(scheme);        		
+        	}
+            return true;
+        }
+        
+        
+        if(action.equals("goAPPurl")) {
+     	   String url = args.getString(0);  
+     	   this.goAppUrl(url);
+        } 
+       
+	    if(action.equals("toAPPopen")) {
+	 	   	String mag = args.getString(0);
+	 	   	Intent i;
+			try {
+				i = Intent.parseUri(mag, Intent.URI_INTENT_SCHEME);
+				this.cordova.getActivity().startActivity(i);
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}      
+	        return true;
+	    }
+	        
+        
+        
         return false;
     }
+
+
+		private void goAppUrl(String message){ 
+			Intent it = new Intent(Intent.ACTION_VIEW); 
+			it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		
+			try {
+				it.setData(Uri.parse("market://details?id="+message));
+				
+			}catch(android.content.ActivityNotFoundException anfe){
+				it.setData(Uri.parse("https://play.google.com/store/apps/details?"+message));
+			}
+		    this.cordova.startActivityForResult(this, it, 0); 
+		    /*
+		      UripackageURI = Uri.parse("package: your.app.id");
+			  Intent intent =new Intent(Intent.ACTION_DELETE);
+		    */
+		}
+
+
     
     public boolean appInstalled(String uri) {
         Context ctx = this.cordova.getActivity().getApplicationContext();
@@ -33,12 +90,12 @@ public class AppChange extends CordovaPlugin {
         return app_installed;
     }
     
-    private void checkAvailability(String uri, CallbackContext callbackContext) {
+    private void checkAvailability(String uri, String qno, CallbackContext callbackContext) {
         if(appInstalled(uri)) {
-            callbackContext.success();
+            callbackContext.success(qno);
         }
         else {
-            callbackContext.error("");
+            callbackContext.error(qno);
         }
     }
 }
