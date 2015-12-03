d4f2cbd6c6368c1f3f29b13c6d8d56a0b98849e3
pearl <lu92500@yahoo.com.tw>
2015-12-03 10:52:04 +0800

    Update AppChange.h

--- src/ios/AppChange.h
+++ src/ios/AppChange.h
@@ -2,8 +2,5 @@
 
 @interface AppChange : CDVPlugin
 
-- (void)check:(CDVInvokedUrlCommand*)command;
-- (void)Inport:(CDVInvokedUrlCommand*)command;
-- (void)toAPPopen:(CDVInvokedUrlCommand*)command;
-- (void)goAPPurl:(CDVInvokedUrlCommand*)command;
+
 @end
