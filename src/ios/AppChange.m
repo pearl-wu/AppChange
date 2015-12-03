ad987968e7dc782a580a991f7b530df0c0bf3881
pearl <lu92500@yahoo.com.tw>
2015-12-03 09:39:12 +0800

    Update AppChange.m

--- src/ios/AppChange.m
+++ src/ios/AppChange.m
@@ -1,14 +1,14 @@
 #import "AppAvailability.h"
 #import <Cordova/CDV.h>
 
-@implementation AppAvailability
+@implementation AppChange
 
-- (void)checkAvailability:(CDVInvokedUrlCommand*)command {
+- (void)check:(CDVInvokedUrlCommand*)command {
     
     CDVPluginResult* pluginResult = nil;
     
     NSString* scheme = [command.arguments objectAtIndex:0];
-    
+	    
     if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:scheme]]) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:(true)];
     }
@@ -18,6 +18,26 @@
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     
+}
+
+- (void)inport:(uri)command{
+	
+}
+
+- (void)toAPPopen:()command{
+	CDVPluginResult* pluginResult = nil;
+    NSString* urischeme = [command.arguments objectAtIndex:0];
+	if ([[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:@urischeme]]) {
+		[[UIApplication sharedApplication]openURL:[NSURL URLWithString:@urischeme]];
+	}
+}
+
+- (void)goAPPurl:()command{
+
+	CDVPluginResult* pluginResult = nil;
+    NSString* appid = [command.arguments objectAtIndex:0];
+	NSString *str = [NSString stringWithFormat:@"http://itunes.apple.com/app/id%d",appid];
+	[[UIApplication sharedApplication] openURL:[NSURL urlWithString:str]];	
 }
 
 @end
