db908edfab76b60fd6cae85443069005467e43af
pearl <lu92500@yahoo.com.tw>
2015-12-04 11:38:26 +0800

    Update AppChange.swift

--- src/ios/AppChange.swift
+++ src/ios/AppChange.swift
@@ -4,12 +4,7 @@ import Foundation
   
     func echo(command: CDVInvokedUrlCommand) {  
   
-        var message = command.arguments[0] as! String  
-        message = message.uppercaseString 
-		// Prove the plugin is actually doing something  
-  
-        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: message)  
-        commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)  
+       
   
     }  
   
