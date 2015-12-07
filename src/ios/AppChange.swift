94dc87f6d0fd9b68855d6df195f538a61ffb5684
pearl <lu92500@yahoo.com.tw>
2015-12-07 15:30:26 +0800

    Update AppChange.swift

--- src/ios/AppChange.swift
+++ src/ios/AppChange.swift
@@ -1,11 +1,11 @@
 import Foundation  
   
-@objc(EchoPlugin) class AppChange : CDVPlugin {  
-  
-    func echo(command: CDVInvokedUrlCommand) {  
-  
-       
-  
-    }  
-  
-}  
+class AppChange : CDVPlugin{
+
+  func check(command: CDVInvokedUrlCommand){
+    var msg = command.arguments[1] as! String
+    var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: msg)
+    commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId)
+  }
+
+}
