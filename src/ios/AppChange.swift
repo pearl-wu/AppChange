5d9c40bde518b903cca3a0b0bc14bb6303f12696
pearl <lu92500@yahoo.com.tw>
2015-12-09 14:27:00 +0800

    Update AppChange.swift

--- src/ios/AppChange.swift
+++ src/ios/AppChange.swift
@@ -1,9 +1,29 @@
-import Foundation  
-  
-class check : CDVPlugin {
-  func check(command: CDVInvokedUrlCommand) {
-    var message = command.arguments[0] as String
-    var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: message)
-    commandDelegate.sendPluginResult(pluginResult, callbackId:command.callbackId)
-  }
+import Foundation
+
+ @objc(HWPAppChange) class AppChange : CDVPlugin {
+    func check(command: CDVInvokedUrlCommand) {
+        var message = command.arguments[0] as! String
+        
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "check \(message)")
+        commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+    }
+    func goAPPurl(command: CDVInvokedUrlCommand) {
+        var message = command.arguments[0] as! String
+        
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "goAPPurl \(message)")
+        commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+    }
+    func toAPPopen(command: CDVInvokedUrlCommand) {
+        var message = command.arguments[0] as! String
+        
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "toAPPopen \(message)")
+        commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+    }
+    func Inport(command: CDVInvokedUrlCommand) {
+        var message = command.arguments[0] as! String
+        
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: "Inport \(message)")
+        commandDelegate!.sendPluginResult(pluginResult, callbackId:command.callbackId)
+    }
+    
 }
