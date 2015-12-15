3d99f6bb7c8b44fc914f59b29a29aeb30bf395aa
pearl <lu92500@yahoo.com.tw>
2015-12-15 17:21:13 +0800

    Update AppChange.swift

--- src/ios/AppChange.swift
+++ src/ios/AppChange.swift
@@ -2,7 +2,7 @@ import Foundation
 
  @objc(HWPAppChange) class AppChange : CDVPlugin {
     func check(command: CDVInvokedUrlCommand) {
-        let id = command.arguments[0] as! String
+       /* let id = command.arguments[0] as! String
         let URLs = command.arguments[1] as! String
         let callsheme:NSURL = NSURL(string: id)!
             if(UIApplication.sharedApplication().canOpenURL(callsheme)){
@@ -11,27 +11,27 @@ import Foundation
             }else{
                 let pluginNO = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: URLs)
                 commandDelegate!.sendPluginResult(pluginNO, callbackId:command.callbackId)
-            }
+            }*/
     }
     func goAPPurl(command: CDVInvokedUrlCommand) {
-        let id = command.arguments[0] as! String
+        /*let id = command.arguments[0] as! String
         let urls:NSURL = NSURL(string: "itms-apps://itunes.apple.com/app/id\(id)")!
         if(UIApplication.sharedApplication().canOpenURL(urls)){
             UIApplication.sharedApplication().openURL(urls)
         }else{
             let pluginNO = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: "INSTALL APP ERROR!!")
             commandDelegate!.sendPluginResult(pluginNO , callbackId: command.callbackId)
-        }
+        }*/
     }
     func toAPPopen(command: CDVInvokedUrlCommand) {
-        let sheme = command.arguments[0] as! String
+      /*  let sheme = command.arguments[0] as! String
         let uris:NSURL = NSURL(string: sheme)!
         if(UIApplication.sharedApplication().canOpenURL(uris)){
             UIApplication.sharedApplication().openURL(uris)
         }else{
             let pluginNO = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: "OPEN APP ERROR!!")
             commandDelegate!.sendPluginResult(pluginNO , callbackId: command.callbackId)
-        }
+        }*/
     }
     func Inport(command: CDVInvokedUrlCommand) {
       // let userDefaults = NSUserDefaults.standardUserDefaults()
