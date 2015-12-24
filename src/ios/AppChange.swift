92085fb0b874d375fd839207c1e86106115e7712
pearl <lu92500@yahoo.com.tw>
2015-12-24 16:44:18 +0800

    Update AppChange.swift

--- src/ios/AppChange.swift
+++ src/ios/AppChange.swift
@@ -34,3 +34,7 @@ import Foundation
         }
     }
 }
+
+func application(application: UIApplication, openURL url: NSURL, sourceApplication: String, annotation: AnyObject?) -> Bool {
+    return true
+}
