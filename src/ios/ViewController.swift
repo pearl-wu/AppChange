81eea4f3ed885d692c8991dffc99df79cd30755f
pearl <lu92500@yahoo.com.tw>
2015-12-24 16:53:51 +0800

    Create ViewController.swift

---
+++ src/ios/ViewController.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+   @IBOutlet weak var txtScheme: UITextField!
+   @IBOutlet weak var txtPath: UITextField!
+   @IBOutlet weak var txtQuery: UITextField!
+
+   override func viewDidLoad() {
+     super.viewDidLoad()
+
+     NSNotificationCenter.defaultCenter().addObserver(
+       self,
+      selector: "displayLaunchDetails",
+      name: UIApplicationDidBecomeActiveNotification,
+      object: nil)
+   }
+
+   func displayLaunchDetails() {
+     var appDelegate =
+       UIApplication.sharedApplication().delegate as AppDelegate
+     if appDelegate.scheme != nil {
+        self.txtScheme.text = appDelegate.scheme
+     }
+     if appDelegate.path != nil {
+        self.txtPath.text = appDelegate.path
+     }
+     if appDelegate.query != nil {
+        self.txtQuery.text = appDelegate.query
+     }
+    }
+
+    deinit {
+      NSNotificationCenter.defaultCenter().removeObserver(
+       self,
+       name: UIApplicationDidBecomeActiveNotification,
+       object: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+    }
+}
